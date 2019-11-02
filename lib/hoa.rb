951e41d0b6edd10de33cb5ec85c81b409e28f3ef
Robert M Ricci <rmricci121@gmail.com>
2019-11-02 11:21:38 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -10,7 +10,7 @@ def add_character(show, name)
   # Should return the array of the 'show' argument
   BASE_HOA[:chipmunks] << "Dave"
   BASE_HOA[:third_earthers] << "Snarf"
-  BASE_HOA[:jetsons]<< " Astro"
+  BASE_HOA[:jetsons]<< "Astro"
   
   return BASE_HOA[show]
 end
